4c9ceecf4964d992d1bdd26b36e58a787e24e498
Moneshai2004 <moneshshanmugam@gmail.com>
2024-04-16 19:29:39 +0530

    first commit

---
+++ frontend/NotesApp/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Login from './pages/Login/Login'; 
+import SignUp from './pages/SignUp/SignUp';
+import Home from './pages/Home/Home';
+
+const routes = (
+  <Router>
+      <Routes>
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signup" exact element={<SignUp />} /> 
+      </Routes>
+    </Router>
+
+
+);
+
+  const App=()=> {
+  return<div>{routes}</div>
+
+  };
+export default App;
