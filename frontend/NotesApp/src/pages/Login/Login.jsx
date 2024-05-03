141368c37365ea8bb728fcddebddc38262b03db7
Moneshai2004 <moneshshanmugam@gmail.com>
2024-05-03 12:45:53 +0530

    login ui

--- frontend/NotesApp/src/pages/Login/Login.jsx
+++ frontend/NotesApp/src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
-
+import PasswordInput from '../../components/input/PasswordInput';
 const Login = () => {
   
 
@@ -14,6 +14,7 @@ const Login = () => {
           <form onSubmit={()=>{}}>
             <h4 className="text-2xl mb-7">Login</h4>
             <input type="email" placeholder="Email" className="input-box" required />
+            <PasswordInput/>
             <button type="submit" className="btn-primary">Login</button>
             <p className="text-sm text-center mt-4">
               Not registered yet? 
