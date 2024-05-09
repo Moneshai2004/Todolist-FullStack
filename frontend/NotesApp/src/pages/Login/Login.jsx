68106f819c36108813688fa9821e1e3805910df2
Moneshai2004 <moneshshanmugam@gmail.com>
2024-05-09 18:56:23 +0530

    login in email er

--- frontend/NotesApp/src/pages/Login/Login.jsx
+++ frontend/NotesApp/src/pages/Login/Login.jsx
@@ -1,24 +1,60 @@
-
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import { Link } from 'react-router-dom';
-import PasswordInput from '../../components/input/PasswordInput';
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 const Login = () => {
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e)=>{
+    e.preventDefault();
+     console.log('Email:', email);
+    console.log('Is email valid?', validateEmail(email));
+    
+    if (!validateEmail(email)){
+      setError("Please enter a valid email address");
+      return;
+    }
+    if (!password){
+      setError("Please enter a password");
+      return;
+    }
+    
+
+    setError("")
+
+    // login api
+  }
 
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={()=>{}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="email" placeholder="Email" className="input-box" required />
-            <PasswordInput/>
-            <button type="submit" className="btn-primary">Login</button>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
             <p className="text-sm text-center mt-4">
-              Not registered yet? 
-              <Link to="/signUp" className="text-blue-500 ml-1">Create an account</Link>
+              Not registered yet?
+              <Link to="/signUp" className="text-blue-500 ml-1">
+                Create an account
+              </Link>
             </p>
           </form>
         </div>
--- frontend/NotesApp/src/pages/SignUp/SignUp.jsx
+++ frontend/NotesApp/src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div><navbar/></div>
+    
   )
 }
 
