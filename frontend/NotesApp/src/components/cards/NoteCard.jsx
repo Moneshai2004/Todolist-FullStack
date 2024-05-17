398b970c18b2d1dd9ee5dafb2fbf52dd800eea50
Moneshai2004 <moneshshanmugam@gmail.com>
2024-05-17 19:06:47 +0530

    Notecard

---
+++ frontend/NotesApp/src/components/cards/NoteCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="">
+        <div>
+            <h6 className="text-sm font-medium">{title}</h6>
+            <span ></span>
+        </div>
+    </div>
+  )
+};
+
+export default NoteCard;
--- frontend/NotesApp/src/pages/Home/Home.jsx
+++ frontend/NotesApp/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/cards/NoteCard'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <div className='container mx-auto'>
+      <NoteCard/>
+    </div>
     </>
   )
 }
