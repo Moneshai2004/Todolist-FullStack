a19f5ef38d0efa45837b330cecb7776bd50050ea
Moneshai2004 <moneshshanmugam@gmail.com>
2024-05-17 18:32:16 +0530

    searchbar

--- frontend/NotesApp/src/components/Navbar/Navbar.jsx
+++ frontend/NotesApp/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import ProfileInfo from '../cards/ProfileInfo';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import ProfileInfo from "../cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [SearchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate(); // Use lowercase 'n' for the hook
 
   const onLogout = () => {
     navigate("/Login"); // Use lowercase 'n' for the function call
   };
+  const handleSearch = ()=>{
+    
+  }
+  const onClearSearch=()=>{
+    setSearchQuery("")
+  }
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow-md">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-      <ProfileInfo onLogout={onLogout}/>
+      <SearchBar
+        value={SearchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
